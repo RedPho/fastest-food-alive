41a369771f35e98e8af7481ab26ad91df7886eac
RedPho <medasdan@gmail.com>
2022-10-24 02:19:43 +0300

    Make menu elements horizontal

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -2,8 +2,9 @@ function createMenuPage() {
     let content = document.querySelector("#content");
     function addFood(name, description, price, imgSrc) {
         let foodCard = document.createElement("div");
-        foodCard.innerHTML = "<img width=150px height=150px src=" + imgSrc + ">" + "<h3>"+ name +"</h3>" + "<p>" + description + "</p>" + "<p><b>" + price + "</b></p>";
+        foodCard.innerHTML = "<img width=150px height=150px src=" + imgSrc + ">" + "<div>" + "<h3>"+ name +"</h3>" + "<p>" + description + "</p>" + "</div>" + "<p><b>" + price + "</b></p>";
         foodCard.classList.add("card");
+        foodCard.classList.add("flex-row");
         content.appendChild(foodCard);
     }
     addFood("Hamburger", "a delicious hamburger", "10$", "../media/hamburger.jpg");
